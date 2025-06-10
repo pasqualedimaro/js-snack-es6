a5607f33f092e6a00f4767335874b18a4afd65a1
pasqualedimaro <pakydm15@gmail.com>
2025-06-10 14:55:47 +0200

    scaffolding and add fisrt array

---
+++ js-snack1/main.js
@@ -0,0 +1,8 @@
+// array di object di bici
+const bici = [
+    { nome: "Bianchi Oltre XR4", peso: 7 },
+    { nome: "Specialized Tarmac", peso: 6 },
+    { nome: "Cannondale SuperSix", peso: 7 },
+    { nome: "Trek Emonda", peso: 8 },
+    { nome: "Pinarello Dogma", peso: 9 }
+];
