c08da3cfbb14cf65f8021c28d88c253496a4b985
pasqualedimaro <pakydm15@gmail.com>
2025-06-10 14:57:39 +0200

    for cicle and stamp

--- js-snack1/main.js
+++ js-snack1/main.js
@@ -6,3 +6,15 @@ const bici = [
     { nome: "Trek Emonda", peso: 8 },
     { nome: "Pinarello Dogma", peso: 9 }
 ];
+
+// trovo bici più leggera con ciclo for
+let biciLeggera = bici[0]; 
+
+for (let i = 1; i < bici.length; i++) {
+    if (bici[i].peso < biciLeggera.peso) {
+        biciLeggera = bici[i];
+    }
+}
+
+// stampo
+console.log(`La bici più leggera è ${biciLeggera.nome} con un peso di ${biciLeggera.peso} kg`)
