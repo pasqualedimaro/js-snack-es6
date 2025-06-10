28dfaaf20b96554099dc343f9ed9f80d1ecf1d6f
pasqualedimaro <pakydm15@gmail.com>
2025-06-10 15:03:23 +0200

    add first array 2nd exercise

---
+++ js-snack2/main.js
@@ -0,0 +1,8 @@
+// creo array di squadre con punti e falli a 0
+const squadre = [
+    { nome: "Napoli", punti: 0, falliSubiti: 0 },
+    { nome: "Inter", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+    { nome: "Roma", punti: 0, falliSubiti: 0 },
+    { nome: "Juventus", punti: 0, falliSubiti: 0 }
+];
